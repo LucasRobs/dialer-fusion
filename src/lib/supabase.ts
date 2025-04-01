182775c6badda7bcdc3c8621b62d025da8539e5e
Bruno Sousa gomes <brunogomes-07@hotmail.com>
2025-04-01 15:56:09 -0300

    supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://pblgifjpehuauriqnjbn.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBibGdpZmpwZWh1YXVyaXFuamJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1Mjk2ODAsImV4cCI6MjA1OTEwNTY4MH0.NPzH-55PWMkinaVtkBiD7AcUGp6itJFK2w3LAKcybzQ';
+const supabaseUrl = 'https://pblgifjpehuauriqnjbn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBibGdpZmpwZWh1YXVyaXFuamJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1Mjk2ODAsImV4cCI6MjA1OTEwNTY4MH0.NPzH-55PWMkinaVtkBiD7AcUGp6itJFK2w3LAKcybzQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
