128433fd4e15b8a176e9d6c29e867a27ae6244f5
Bruno Sousa gomes <brunogomes-07@hotmail.com>
2025-04-01 15:04:32 -0300

    database

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ovanntvqwzifxjrnnalr.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92YW5udHZxd3ppZnhqcm5uYWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMTE4NzgsImV4cCI6MjA1ODU4Nzg3OH0.t7R_EiadlDXqWeB-Sgx_McseGGkrbk9br_mblC8unK8";
+const SUPABASE_URL = "https://pblgifjpehuauriqnjbn.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBibGdpZmpwZWh1YXVyaXFuamJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1Mjk2ODAsImV4cCI6MjA1OTEwNTY4MH0.NPzH-55PWMkinaVtkBiD7AcUGp6itJFK2w3LAKcybzQ";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
